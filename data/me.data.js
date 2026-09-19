665d4b723a891d3280bee00fd2f74bd3ca2b99fd
Diaa-I <117300163+Diaa-I@users.noreply.github.com>
2026-09-19 15:56:22 +0400

    Fixing start information

--- data/me.data.js
+++ data/me.data.js
@@ -3,8 +3,8 @@ const MeData =
   name: "Diaa Ibrahim Nasr",
   title: "Backend-Focused Full-Stack Developer / Ethical Hacker",
   summary: "Backend-Focused Full-Stack Developer and Ethical Hacker building secure applications in Python, Node.js, and PHP. Currently a Hardware Security Research Assistant at the University of Sharjah analyzing hardware-software vulnerabilities, while independently training in offensive web hacking labs.",
-  education: "Bachelor of Science in Computer Science from the University of Sharjah (2024), followed by a Graduate Professional Diploma in Cybersecurity from the American University of Beirut (2026)",
-  interests: 'Secure backend development, red teaming, web application hacking, and hardware exploitation.',
+  education: "EDU: Bachelor of Science in Computer Science from the University of Sharjah (2024), followed by a Graduate Professional Diploma in Cybersecurity from the American University of Beirut (2026).",
+  interests: 'Focus areas: Secure backend development, red teaming, web application hacking, and hardware exploitation.',
   id: 1,
 }
 
